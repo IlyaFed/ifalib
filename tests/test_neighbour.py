ee245081015ed755b2d8bc11778252f689f7bcc7
IlyaFed <ilya.d.fedorov@phystech.edu>
2021-06-29 23:38:27 +0300

    New commit

--- tests/test_neighbour.py
+++ tests/test_neighbour.py
@@ -42,3 +42,44 @@ def test_h_h2_structures():
     print (returned_dict)
     assert(returned_dict['H1']['count'][0]==Npart/3)
     assert(returned_dict['H2']['count'][0]==Npart/3)
+
+def test_h_h2_h3_h4_structures():
+    Rcut = 1.0
+    Cell = 20.0
+    dCell = 2.0
+    Nsteps = 2
+    Maxunique = 10
+    NinCell = int(Cell/dCell)
+    Npart = int(NinCell**3*1.5)+3
+    Coords = []
+    Types = []
+    for step in range(Nsteps):
+        Coord_point = []
+        for xi in range(NinCell):
+            for yi in range(NinCell):
+                for zi in range(NinCell):
+                    # H-H distance 0.75, about (0.2^2+0.2^2+0.2^2) x 2
+                    # H1
+                    Coord_point.append([xi*dCell + 0.2, yi*dCell + 0.2, zi*dCell + 0.2])
+                    Types.append("H")
+                    # H2
+                    if (len(Coord_point) > Npart/3):
+                        Coord_point.append([xi*dCell - 0.2, yi*dCell - 0.2, zi*dCell - 0.2])
+                        Types.append("H")
+                    # H3
+                    if (len(Coord_point) >= Npart - 1):
+                        Coord_point.append([xi*dCell + 0.2, yi*dCell - 0.2, zi*dCell - 0.2])
+                        Types.append("H")
+                    # H4
+                    if (len(Coord_point) == Npart):
+                        Coord_point.append([xi*dCell - 0.2, yi*dCell - 0.2, zi*dCell + 0.2])
+                        Types.append("H")
+        Coords.append(Coord_point)
+
+
+    returned_dict = neighbour.neighbour(Coords, Types, Cell, Rcut, Maxunique)
+    print (returned_dict)
+    assert(returned_dict['H1']['count'][0]==Npart/3)
+    assert(returned_dict['H2']['count'][0]==Npart/3)
+    assert(returned_dict['H3']['count'][0]==1)
+    assert(returned_dict['H4']['count'][0]==1)
